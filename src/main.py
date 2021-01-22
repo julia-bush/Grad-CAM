68ed7351bef556774521ad57307166765a6ace02
j <jay.arch@gmail.com>
2021-01-22 15:24:32 +0000

    tidy up

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from pathlib import Path
 
 import cv2
@@ -13,7 +12,6 @@ from tensorflow.keras.applications.vgg16 import (
     decode_predictions,
 )
 
-
 # GPU config works for both one or two GPUs
 physical_devices = tf.config.experimental.list_physical_devices("GPU")
 assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
@@ -23,16 +21,18 @@ set_session(sess)
 
 tf.compat.v1.disable_eager_execution()
 
-# test_dir = sys.argv[1]
+"""
+Specify directory containing images you want to plot grad-CAM heatmaps for.
+Uses VGG16 pre-trained on ImageNet, 1000 classes.
+"""
+
 dataset_name = "viaducts"
-# test_dir = Path.cwd().parent / "data" / dataset_name
-# pred_dir = Path.cwd().parent / "predictions" / dataset_name
 test_dir = Path.cwd().parent / "data" / dataset_name
 pred_dir = Path.cwd().parent / "predictions" / dataset_name
 Path(pred_dir).mkdir(parents=True, exist_ok=True)
 
 model = VGG16(weights="imagenet")
-no_classes=1000
+no_classes = 1000
 
 for filename in os.listdir(test_dir):
 
@@ -78,15 +78,5 @@ for filename in os.listdir(test_dir):
         1,
         cv2.LINE_AA,
     )
-    # cv2.imwrite("gradcam.jpg", cam)
-    cv2.imwrite(f"{pred_dir}/{Path(filename).stem}_gradcam.jpg", cam)
 
-    # register_gradient()
-    # guided_model = modify_backprop(model, "GuidedBackProp")
-    # saliency_fn = compile_saliency_function(guided_model)
-    # saliency = saliency_fn([preprocessed_input, 0])
-    # gradcam = saliency[0] * heatmap[..., np.newaxis]
-    # # cv2.imwrite("guided_gradcam.jpg", deprocess_image(gradcam))
-    # cv2.imwrite(
-    #     pred_dir + Path(filename).stem + "_guided_gradcam.jpg", deprocess_image(gradcam)
-    # )
+    cv2.imwrite(f"{pred_dir}/{Path(filename).stem}_gradcam.jpg", cam)
