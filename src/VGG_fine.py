d0fb55494e9437b793ba837522d894b582bc27dc
j <jay.arch@gmail.com>
2021-01-22 20:15:56 +0000

    param change

--- src/VGG_fine.py
+++ src/VGG_fine.py
@@ -87,7 +87,7 @@ def run():
 
     model.compile(
         loss="categorical_crossentropy",
-        optimizer=optimizers.RMSprop(lr=1e-4),
+        optimizer=optimizers.Adam(lr=1e-6),
         metrics=["acc"],
     )
 
