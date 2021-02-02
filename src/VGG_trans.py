10fb3cc1ae9d2585d6c5f6633c64443ec7e6aa85
j <jay.arch@gmail.com>
2021-02-02 07:28:33 +0000

    param change

--- src/VGG_trans.py
+++ src/VGG_trans.py
@@ -59,7 +59,7 @@ def run():
     train_datagen = ImageDataGenerator(rescale=1.0 / 255, validation_split=0.2)
 
     # Change the batchsize according to your system RAM
-    train_batchsize = 64
+    train_batchsize = 32
     val_batchsize = 32
     epochs = 10
 
