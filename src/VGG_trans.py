4ab235c8034797c8af068198c1447d36f1abec01
j <jay.arch@gmail.com>
2021-03-08 10:00:42 +0000

    HE defects

--- src/VGG_trans.py
+++ src/VGG_trans.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import numpy as np
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator
