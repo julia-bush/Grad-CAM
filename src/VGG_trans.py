7024afb08298e09ff5540648a6fa58861244e95e
j <jay.arch@gmail.com>
2021-03-17 20:51:45 +0000

    multi_class_HE

--- src/VGG_trans.py
+++ src/VGG_trans.py
@@ -15,7 +15,7 @@ def run():
     physical_devices = tf.config.experimental.list_physical_devices("GPU")
     assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
     config = tf.compat.v1.ConfigProto()
-    sess = tf.compat.v1.Session(config=config)
+    sess = tf.compat.v1.Session
     set_session(sess)
 
     n_classes = 11
