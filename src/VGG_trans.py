3e52021c0ced093b8f585b80c4805f7f060723e9
j <jay.arch@gmail.com>
2021-03-19 20:22:58 +0000

    param change

--- src/VGG_trans.py
+++ src/VGG_trans.py
@@ -66,7 +66,7 @@ def run():
     # Change the batchsize according to your system RAM
     train_batchsize = 32
     val_batchsize = 32
-    epochs = 1
+    epochs = 20
 
     # Data generator for training data
     train_generator = train_datagen.flow_from_directory(
