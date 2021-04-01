f498272c8916bddc7fe075b0ba93512e607d342b
j <jay.arch@gmail.com>
2021-04-01 14:32:57 +0100

    Implementation of Grad-CAM using tf.GradientTape and the last dense layer of the classifier head (instead of softmax whose near-zero probabilities are not helpful for gradient calcs).

---
+++ src/grad_cam.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+
+import numpy as np
+import tensorflow as tf
+from scipy.special import softmax
+from tensorflow import keras
+
+from grad_cam_utils import get_img_array, make_gradcam_heatmap, colour_heatmap, superimpose_heatmap, save_superimposed_heatmap
+
+"""
+Once you have a trained classifier model, you can run classification predictions and visualise which image regions have
+been the most influential for a particular predicted class using Grad-CAM. Place the images to classify in test_dir,
+specify: pred_dir, trained_model, class_labels, img_size, last_conv_layer_name, preprocess_input, and hit play.
+For reference:
+https://keras.io/examples/vision/grad_cam/
+https://www.tensorflow.org/guide/autodiff
+"""
+
+dataset_name = "HE_defects_sample"
+test_dir = Path.cwd().parent / "data" / dataset_name
+pred_dir = Path.cwd().parent / "predictions" / dataset_name / "trans_CAM"
+Path(pred_dir).mkdir(parents=True, exist_ok=True)
+trained_model = f"{Path.cwd().parent}/models/VGG_trans-multiclass_main.hdf5"
+
+model = tf.keras.models.load_model(trained_model)
+model.layers[-1].activation = None  # remove softmax to circumvent near-zero probabilities
+class_labels = ["corrosion", "crack", "spalling"]  # TODO: read these from file saved during model training
+img_size = (224, 224)
+preprocess_input = keras.applications.vgg16.preprocess_input
+last_conv_layer_name = "block5_conv3"
+
+for filename in os.listdir(test_dir):
+
+    img_path = f"{test_dir}/{filename}"
+    img_array = preprocess_input(get_img_array(img_path, size=img_size))
+
+    preds = model.predict(img_array)
+    probas = softmax(preds)
+    top_two_preds_indices = np.argsort(-preds)[0][:2]
+
+    for idx, pred_index in enumerate(top_two_preds_indices):
+        cam_path = f"{pred_dir}/{idx}_{filename}"  # TODO: decide how to index filenames for different predicted classes
+        heatmap = make_gradcam_heatmap(img_array=img_array, model=model, last_conv_layer_name=last_conv_layer_name, pred_index=pred_index)
+        legend = f"Predicted {class_labels[pred_index]} with probability {probas[0][pred_index]:.4f}"
+        save_superimposed_heatmap(img_path=img_path, heatmap=heatmap, cam_path=cam_path, legend=legend)
---
+++ src/grad_cam_utils.py
@@ -0,0 +1,70 @@
+import numpy as np
+import tensorflow as tf
+from PIL import ImageDraw, ImageFont
+from matplotlib import cm as cm
+from tensorflow import keras
+
+
+def get_img_array(img_path, size):
+    img = keras.preprocessing.image.load_img(img_path, target_size=size)
+    array = keras.preprocessing.image.img_to_array(img)
+    array = np.expand_dims(array, axis=0)
+    return array
+
+
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+    # collect the necessary tf.Variables into keras.Model, subclass of tf.Module, for convenience
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    # record forward pass operations on a tf.GradientTape for automatic differentiation
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]  # chosen class prediction before softmax TODO: better var name
+
+    # d(class_channel)/d(last_conv_layer_output), i.e.
+    # how sensitive is class_channel to changes in last_conv_layer_output
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+    # take the mean of gradients over every response map
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    last_conv_layer_output = last_conv_layer_output[0]
+    # heatmap is the weighted (by mean gradient) sum of response maps in chosen layer
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+    # normalise between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+
+    return heatmap.numpy()
+
+
+def colour_heatmap(heatmap, colourmap="jet"):
+    scaled_heatmap = np.uint8(255 * heatmap)
+    cmap = cm.get_cmap(colourmap)
+    cmap_colors = cmap(np.arange(256))[:, :3]
+    cmap_heatmap = cmap_colors[scaled_heatmap]
+    return cmap_heatmap
+
+
+def superimpose_heatmap(img_path, heatmap, alpha):
+    img = keras.preprocessing.image.load_img(img_path)
+    img = keras.preprocessing.image.img_to_array(img)
+    heatmap = keras.preprocessing.image.array_to_img(heatmap)
+    heatmap = heatmap.resize((img.shape[1], img.shape[0]))
+    heatmap = keras.preprocessing.image.img_to_array(heatmap)
+    superimposed_img = heatmap * alpha + img
+    superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+    return superimposed_img
+
+
+def save_superimposed_heatmap(img_path, heatmap, cam_path, colourmap="jet", alpha=0.4, legend=None):
+    cmap_heatmap = colour_heatmap(heatmap=heatmap, colourmap=colourmap)
+    superimposed_img = superimpose_heatmap(img_path=img_path, heatmap=cmap_heatmap, alpha=alpha)
+    if legend is not None:
+        draw = ImageDraw.Draw(superimposed_img)
+        font = ImageFont.truetype("arial.ttf", 18)
+        draw.text((5, 5), legend, (255, 255, 255), font=font)
+    superimposed_img.save(cam_path)
